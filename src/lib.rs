b8b7228ffd6f15ef427fc561b55bfddafdb13cb5
neckaros <arnaud.jezequel@gmail.com>
2024-12-12 15:01:39 +0100

    Error Management

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,33 @@
-use std::{fs::File, io::Read, path::PathBuf};
-
+use std::{fs::File, io::{self, Read}, path::PathBuf};
 use data_encoding::BASE32;
+use derive_more::derive::From;
 use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
 use sha1::{Sha1, Digest};
 use urlencoding::encode;
+
+#[serde_as]
+#[derive(Debug, Serialize, From, strum_macros::AsRefStr)]
+#[serde(tag = "type", content = "data")]
+pub enum RsTorrentMagnetError {
+    DecodeError,
+    #[from]
+	Io(#[serde_as(as = "DisplayFromStr")] std::io::Error),
+    #[from]
+	BtEncode(#[serde_as(as = "DisplayFromStr")] bt_bencode::Error)
+}
+impl core::fmt::Display for RsTorrentMagnetError {
+	fn fmt(
+		&self,
+		fmt: &mut core::fmt::Formatter,
+	) -> core::result::Result<(), core::fmt::Error> {
+		write!(fmt, "{self:?}")
+	}
+}
+
+impl std::error::Error for RsTorrentMagnetError {}
+
+type RsTorrentMagnetResult<T> = Result<T, RsTorrentMagnetError>;
 
 /// Parsed torrent file
 #[derive(Debug, Deserialize, Serialize)]
@@ -76,40 +100,40 @@ where
     Ok(s.to_vec())
 }
 
-pub fn decode_file(path: PathBuf) -> MagnetDetail {
-    let mut file = File::open(path).unwrap();
+pub fn decode_file(path: PathBuf) -> RsTorrentMagnetResult<MagnetDetail> {
+    let mut file = File::open(path)?;
     let mut encoded = Vec::new();
-    file.read_to_end(&mut encoded).unwrap();
+    file.read_to_end(&mut encoded)?;
     decode(encoded)
 }
 
 
-pub fn decode(encoded: Vec<u8>) -> MagnetDetail {
-    let info: Torrent = bt_bencode::from_slice(&encoded).unwrap();
+pub fn decode(encoded: Vec<u8>) -> RsTorrentMagnetResult<MagnetDetail> {
+    let info: Torrent = bt_bencode::from_slice(&encoded)?;
 
-    let info_raw: TorrentRaw = bt_bencode::from_slice(&encoded).unwrap();
+    let info_raw: TorrentRaw = bt_bencode::from_slice(&encoded)?;
 
     let mut hasher = Sha1::new();
     hasher.update(info_raw.info);
     let result = hasher.finalize();
     let r = BASE32.encode(&result);
 
-    MagnetDetail {
+    Ok(MagnetDetail {
         torrent: info,
         hash: r
-    }
+    })
 }
 
 /// Get magnet from a torrent file
-pub fn magnet_from_torrent_file(path: PathBuf) -> String {
-    let infos = decode_file(path);
-    infos.as_magnet()
+pub fn magnet_from_torrent_file(path: PathBuf) -> RsTorrentMagnetResult<String> {
+    let infos = decode_file(path)?;
+    Ok(infos.as_magnet())
 }
 
 /// Get magnet from a torrent buffer
-pub fn magnet_from_torrent(data: Vec<u8>) -> String {
-    let infos = decode(data);
-    infos.as_magnet()
+pub fn magnet_from_torrent(data: Vec<u8>) -> RsTorrentMagnetResult<String> {
+    let infos = decode(data)?;
+    Ok(infos.as_magnet())
 }
 
 #[cfg(test)]
@@ -121,7 +145,7 @@ mod tests {
     #[test]
     fn parse_file() {
         let path = PathBuf::from_str("test_data/big-buck-bunny.torrent").unwrap();
-        let result = magnet_from_torrent_file(path);
+        let result = magnet_from_torrent_file(path).unwrap();
         assert_eq!(result, "magnet:?xt=urn:btih:3WBFL3G4PSSV7MF37AJSHWDQMLNR63I4&dn=Big%20Buck%20Bunny&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.fastcast.nz");
     }
 
@@ -131,14 +155,14 @@ mod tests {
         let mut encoded = Vec::new();
         file.read_to_end(&mut encoded).unwrap();
 
-        let result = magnet_from_torrent(encoded);
+        let result = magnet_from_torrent(encoded).unwrap();
         assert_eq!(result, "magnet:?xt=urn:btih:3WBFL3G4PSSV7MF37AJSHWDQMLNR63I4&dn=Big%20Buck%20Bunny&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.fastcast.nz");
     }
 
     #[test]
     fn parse_torrent() {
         let path = PathBuf::from_str("test_data/big-buck-bunny.torrent").unwrap();
-        let result = decode_file(path);
+        let result = decode_file(path).unwrap();
         assert_eq!(result.hash, "3WBFL3G4PSSV7MF37AJSHWDQMLNR63I4");
         assert_eq!(result.torrent.info.name, Some("Big Buck Bunny".to_owned()));
     }
